c890e02d565a9bc781ccb7f2cb97eb4b0939d13a
Akash99-eng <80410694+Akash99-eng@users.noreply.github.com>
2021-03-27 21:44:32 +0530

    Create Multiplication of two number.c

---
+++ Multiplication of two number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+int a=20,b=30;
+int mult;
+mult=a*b;
+    int num = 1;
+
+    switch (num) {
+        case 1:
+        printf("%d", mult);
+        break;
+        }
+        }
